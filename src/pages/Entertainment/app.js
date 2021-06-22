d3e6574bc0d0e4a2d2ab3d2358cccc3743ccc63a
thiagohc20 <thiago-henrique40@hotmail.com>
2021-06-22 00:59:42 -0300

    teste2

---
+++ src/pages/Entertainment/app.js
@@ -0,0 +1,40 @@
+$(function(){
+    $(".popular_shows").slick({
+        infinite: false,
+        slidesToShow: 5,
+        slidesToScroll: 5
+      });
+
+})
+
+$(function(){
+  $(".animation").slick({
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 5
+  })
+})
+
+$(function(){
+  $(".drama").slick({
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+  })
+})
+
+$(function(){
+  $(".unscripted").slick({
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 5
+  })
+})
+
+$(function(){
+  $(".comedy").slick({
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 5
+  })
+})
